4b44851b6fe3689b6b796a22b54d2ac9e9f80824
Jinwoo Yu <phoenix9373@naver.com>
2021-08-31 16:30:18 +0900

    add router 설정

--- src/app.js
+++ src/app.js
@@ -1,4 +1,37 @@
-import * as math from "./math.js";
-import '../style.css'
+"use strict";
 
-math.sum(1, 2);
+import "../style.css";
+
+const routes = {
+  "/home": "Home",
+  "/about": "About",
+  "/project": "Project",
+};
+
+const $div = document.querySelector("#root");
+
+// pushstate, onpopstate
+window.onload = () => {
+  const historyLinker = document.querySelector("ul");
+
+  historyLinker.addEventListener("click", (event) => {
+    if (event.target === event.currentTarget) return;
+
+    const pathName = event.target.getAttribute("route");
+
+    historyRouterPush(pathName, $div);
+  });
+};
+
+window.onpopstate = () => {
+  renderHTML($div, window.location.pathname);
+};
+
+const historyRouterPush = (pathName, element) => {
+  window.history.pushState({}, pathName, window.location.origin + pathName);
+  renderHTML(element, routes[pathName]);
+};
+
+const renderHTML = (element, route) => {
+  element.innerHTML = route;
+};
